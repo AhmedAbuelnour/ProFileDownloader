e9e50ca247a7e7a829a100e57e1387cba5d35ead
Ahmed Abu El-Nour <AhmedAbuElNoor@outlook.com>
2019-01-28 21:38:06 +0200

    Add files via upload

---
+++ NetworkFile/FileSegmentationExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace ProFileDownloader.NetworkFile
+{
+    using System;
+    using System.IO;
+    using System.IO.Pipelines;
+    using System.Threading;
+    using System.Threading.Tasks;
+    internal static class FileSegmentationExtensions
+    {
+        internal static async Task DownloadSegmentAsync(this FileSegment segment, Action<float> CurrentProgress, CancellationToken cancellationToken = default)
+        {
+            Pipe Pipeline = new Pipe();
+
+            await Task.WhenAll(Task.Run(async () =>
+            {
+                int bytesRead;
+                while ((bytesRead = await segment.PartialContent.ReadAsync(Pipeline.Writer.GetMemory(), cancellationToken)) > 0) // Where the downloading part is happening
+                {
+                    segment.TotalReadBytes += bytesRead;
+                    CurrentProgress.Invoke
+                    (
+                         ((((segment.Start + segment.TotalReadBytes) - segment.Start) / ((float)(segment.End - segment.Start))) * 100)
+                    ); // To Get the current percentage.
+                    Pipeline.Writer.Advance(bytesRead);
+                    var flushResult = await Pipeline.Writer.FlushAsync(cancellationToken);
+                    if (flushResult.IsCanceled) break;
+                    if (flushResult.IsCompleted) break;
+                }
+                Pipeline.Writer.Complete();
+            }, cancellationToken)
+                , Task.Run(async () =>
+                {
+                    using (Stream TempLocalFile = new FileStream(segment.TempFile, FileMode.Append, FileAccess.Write))
+                    {
+                        TempLocalFile.Seek(TempLocalFile.Length, SeekOrigin.Begin);
+                        while (true)
+                        {
+                            var ReadResult = await Pipeline.Reader.ReadAsync(cancellationToken);
+                            foreach (var buffer in ReadResult.Buffer)
+                            {
+                                await TempLocalFile.WriteAsync(buffer, cancellationToken);
+                            }
+
+                            Pipeline.Reader.AdvanceTo(ReadResult.Buffer.End);
+
+                            if (ReadResult.IsCompleted || ReadResult.IsCanceled)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    Pipeline.Reader.Complete();
+                }, cancellationToken));
+        }
+    }
+}
---
+++ NetworkFile/UtilityFiles.cs
@@ -0,0 +1,32 @@
+﻿namespace ProFileDownloader.NetworkFile
+{
+    using Newtonsoft.Json;
+    using System.IO;
+
+    internal class ServerFile
+    {
+        internal string Name { get;  set; }
+        internal string Extension { get;  set; }
+        internal long Size { get;  set; }
+        internal string MediaType { get;  set; }
+        internal Stream DownloadContent { get;  set; }
+        internal bool IsResumable { get;  set; }
+        internal long TotalReadBytes { get; set; }
+    }
+
+    internal class FileSegment
+    {
+        [JsonProperty]
+        public int ID { get; set; }
+        [JsonIgnore]
+        public Stream PartialContent { get; set; }
+        [JsonProperty]
+        public long Start { get; set; }
+        [JsonProperty]
+        public long End { get; set; }
+        [JsonProperty]
+        public string TempFile { get; set; } = Path.GetTempFileName();
+        [JsonIgnore]
+        public long TotalReadBytes { get; set; } 
+    }
+}
